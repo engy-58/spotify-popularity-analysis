1d4c15823fa91223a70a0ff49ad4557f3da17cf5
Your Nameengy <engy.ahmed1167@gmail.com>
2023-09-14 13:35:09 +0300

    Created "requirements.txt".

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 project_dir = os.getcwd()
 subdirs = ['data/raw_data', 'data/processed_data', 'notebooks']
 
-# Create subdirectories 
+# Create subdirectories
 for subdir in subdirs:
     subdir_path = os.path.join(project_dir, subdir)
     if not os.path.exists(subdir_path):
@@ -16,5 +16,16 @@ notebook_path = os.path.join(project_dir, 'notebooks', notebook_name)
 if not os.path.exists(notebook_path):
     with open(notebook_path, 'w') as nb:
         nb.write(f'# Spotify Popularity Analysis\n\n')
+
+# Create a requirements.txt file and add dependencies
+with open(os.path.join(project_dir, 'requirements.txt'), 'w') as requirements_file:
+    requirements_file.write("pandas\n")
+    requirements_file.write("numpy\n")
+    requirements_file.write("matplotlib\n")
+    requirements_file.write("seaborn\n")
+    requirements_file.write("plotly\n")
+    requirements_file.write("scikit-learn\n")
+    requirements_file.write("ipywidgets\n")
+    requirements_file.write("statsmodels\n")  
 
 
